fe3916388c3f2857cc6f483f8d1716dde0adcb18
Rahul Prasad <rahul.prsd.in@gmail.com>
2022-02-14 23:27:19 +0530

    Create UnionOfArray.java

---
+++ UnionOfArray.java
@@ -0,0 +1,16 @@
+class Solution{
+    public static int doUnion(int a[], int n, int b[], int m) 
+    {
+        Set<Integer> set = new HashSet<Integer>();
+        for(int i=0; i<n;i++){
+            set.add(a[i]);
+        }
+        
+        for(int i=0; i<m;i++){
+            set.add(b[i]);
+        }
+        
+        return set.size();
+    }
+    
+}
